2811b5f18dbfe4d0011f806725b43523ecc12eab
Breno Scudo <brenoscudo@hotmail.com>
2024-06-11 09:39:44 +0200

    initial commit

---
+++ src/main/java/org/generation/italy/mtfuji/MtfujiApplication.java
@@ -0,0 +1,13 @@
+package org.generation.italy.mtfuji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtfujiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtfujiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/italy/mtfuji/MtfujiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.italy.mtfuji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtfujiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
